ba512aaff35be14678922c6770470df01e1ef846
Gustavo Lopes <lopes.gustavojf@gmail.com>
2020-06-24 19:18:47 -0300

    Expose CSV Reader so it can be edited like original csv reader

--- parser/structCsvParser.go
+++ parser/structCsvParser.go
@@ -12,48 +12,12 @@ import (
 )
 
 type Parser struct {
-	reader  *csv.Reader
+	Reader  *csv.Reader
 	header  []string
 	options Options
 }
 
 type Options struct {
-	// Comma is the field delimiter.
-	// It is set to comma (',') by NewReader.
-	// Comma must be a valid rune and must not be \r, \n,
-	// or the Unicode replacement character (0xFFFD).
-	Comma rune
-
-	// Comment, if not 0, is the comment character. Lines beginning with the
-	// Comment character without preceding whitespace are ignored.
-	// With leading whitespace the Comment character becomes part of the
-	// field, even if TrimLeadingSpace is true.
-	// Comment must be a valid rune and must not be \r, \n,
-	// or the Unicode replacement character (0xFFFD).
-	// It must also not be equal to Comma.
-	Comment rune
-
-	// FieldsPerRecord is the number of expected fields per record.
-	// If FieldsPerRecord is positive, Read requires each record to
-	// have the given number of fields. If FieldsPerRecord is 0, Read sets it to
-	// the number of fields in the first record, so that future records must
-	// have the same field count. If FieldsPerRecord is negative, no check is
-	// made and records may have a variable number of fields.
-	FieldsPerRecord int
-
-	// If LazyQuotes is true, a quote may appear in an unquoted field and a
-	// non-doubled quote may appear in a quoted field.
-	LazyQuotes bool
-
-	// If TrimLeadingSpace is true, leading white space in a field is ignored.
-	// This is done even if the field delimiter, Comma, is white space.
-	TrimLeadingSpace bool
-
-	// ReuseRecord controls whether calls to Read may return a slice sharing
-	// the backing array of the previous call's returned slice for performance.
-	// By default, each call to Read returns newly allocated memory owned by the caller.
-	ReuseRecord bool
-
 	UseHeader  bool
 	TimeLayout string
 }
@@ -78,7 +42,7 @@ func New(inputStream io.Reader, options Options) Parser {
 	//csvReader.ReuseRecord = options.ReuseRecord
 
 	return Parser{
-		reader:  csvReader,
+		Reader:  csvReader,
 		header:  header,
 		options: options,
 	}
